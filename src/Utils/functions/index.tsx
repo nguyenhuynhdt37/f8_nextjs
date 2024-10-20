77889248750e0bf340752124ed2baace38dcc819
= <=>
2024-10-20 09:34:24 +0700

    nạkdhjas

--- src/Utils/functions/index.tsx
+++ src/Utils/functions/index.tsx
@@ -2,3 +2,16 @@ export function isValidEmail(email: string): boolean {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+export const formatCurrency = (amount: number) => {
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(amount);
+};
+export const getVideoIdFromUrl = (url: string) => {
+  if (url == null) return null;
+  const match = url.match(
+    /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([^&]+)/
+  );
+  return match ? match[1] : null;
+};
